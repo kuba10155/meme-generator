90be579ef0cfd197c6fe60c594b780f8f587c575
kuba10155 <j.zborowski24@gmail.com>
2023-02-05 22:42:51 +0100

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
   return (
     <header className="header">
-      <img className="header--image" src="images/troll-face.png"/>
+      <img className="header--image" src="./images/troll-face.png"/>
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--text">Jakub Zborowski</h4>
     </header>
